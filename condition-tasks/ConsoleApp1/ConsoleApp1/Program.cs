4c3f5f1e4ba75b3ba3e203059c7863784afd2958
Lumi Liikanen <lumi_liikanen@hotmail.com>
2018-09-19 09:20:44 +0300

    Condition folder defined

---
+++ condition-tasks/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IfTask1
+{//this one line komment
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma selvittää onko annettu luku positiivinen, negatiivinen vai nolla");
+        }Console.WriteLine(")
+    }
+}
