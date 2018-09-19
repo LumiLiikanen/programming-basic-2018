629172ede80f11652b909d2b57806fa36bbc6669
Lumi Liikanen <lumi_liikanen@hotmail.com>
2018-09-19 10:00:34 +0300

    Task 1 done

--- condition-tasks/ConsoleApp1/ConsoleApp1/Program.cs
+++ condition-tasks/ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,6 +7,17 @@ namespace IfTask1
         static void Main(string[] args)
         {
             Console.WriteLine("Ohjelma selvittää onko annettu luku positiivinen, negatiivinen vai nolla");
-        }Console.WriteLine(")
+            Console.Write("syötä numero: ");
+            string userInput = Console.ReadLine();
+            int number = int.Parse(userInput);
+
+            if (number == 0)
+                Console.WriteLine($"numero {number} on nolla!");
+            else if (number < 0)
+                Console.WriteLine($"numero {number} on negatiivinen!");
+            else
+                Console.WriteLine($"numero {number} on positiivinen!");
+                Console.WriteLine($"syötit numeron {userInput}");
+        }
     }
 }
