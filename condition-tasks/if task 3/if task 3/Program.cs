2cf34673afd9c826fe0a725e9c42a7017f74ef3e
Lumi Liikanen <lumi_liikanen@hotmail.com>
2018-09-20 09:38:35 +0300

    If task 1 done

---
+++ condition-tasks/if task 3/if task 3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace if_task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ohjelma kertoo onko luku parillinen vai pariton ja onko se negatiivinen, positiivinen vai nolla");
+            Console.Write("Syötä numero: ");
+            string userInput = Console.ReadLine();
+            int number = int.Parse(userInput);
+
+            if (number == 0)
+                Console.WriteLine($"numero {number} on nolla!");
+            else if (number < 0)
+                Console.WriteLine($"numero {number} on negatiivinen!");
+            else if (number > 0)
+                Console.WriteLine($"numero {number} on positiivinen!");
+            Console.WriteLine($"syötit numeron {userInput}");
+
+        }
+    }
+}
