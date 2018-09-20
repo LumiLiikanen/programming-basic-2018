9ed335280ba9421408f02f48abf5157fb47ea7f6
Lumi Liikanen <lumi_liikanen@hotmail.com>
2018-09-20 09:01:56 +0300

    If task 2 version 1

--- condition-tasks/if task 2/if task 2/Program.cs
+++ condition-tasks/if task 2/if task 2/Program.cs
@@ -6,7 +6,17 @@ namespace if_task_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("ohjelma selvittää onko luku pariton vai parillinen");
+            Console.Write("syötä numero: ");
+            String userInput = Console.ReadLine();
+            int number = int.Parse(userInput);
+
+            if (number % 2 == 0)
+                Console.WriteLine($"luku {number} on parillinen");
+            else
+                Console.WriteLine($"luku {number} on pariton");
+
+
         }
     }
 }
