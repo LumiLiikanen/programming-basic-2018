ad33c33c8ec707ce027fa51f65ba7e86d07184fe
Lumi Liikanen <lumi_liikanen@hotmail.com>
2018-10-11 10:06:52 +0300

    Program done (?)

--- condition-tasks/if task lipun hinta/if task lipun hinta/if task lipun hinta/Program.cs
+++ condition-tasks/if task lipun hinta/if task lipun hinta/if task lipun hinta/Program.cs
@@ -6,48 +6,56 @@ namespace if_task_lipun_hinta
     {
         static void Main(string[] args)
         {
-            Console.OutputEncoding = System.Text.Encoding.UTF8;
             Console.WriteLine("Ohjelma laskee lipun hinnan!");
-            int ticketPrise = 16;
-            double discount1 = 1.0;
-            double discount2 = 0.5;
-            double discount3 = 0.45 * 0.15;
-            double discount4 = 0.15;
-            
-            Console.WriteLine("Oletko < 7 vai 7-65 vai >65 -vuotias?");
+            int TicketPrise = 16;
+            double discount = 0;
+
+            Console.Write("Oletko varusmies? k tai e");
             string userInput = Console.ReadLine();
-            int kylla = int.Parse(userInput);
-            if (userInput == "< 7")
-                Console.WriteLine($"Lipunhintasi on {ticketPrise - ticketPrise * discount1}€");
-            else if > 65
-                Console.WriteLine($"Lipunhintasi on {ticketPrise - ticketPrise * discount2}€");
-            else if 7 - 65
-               Console.WriteLine("Oletko opiskelija kylla/ei?");
-            string userInput = Console.ReadLine();
-            int kylla = int.Parse(userInput);
-            { if kylla
-                Console.WriteLine("Oletko myös MTK:n jäsen kylla/ei?);
-                    String userInput = Console.ReadLine();
-                int kylla = int.Parse(userInput);
-                if kylla
-                    Console.WriteLine($"Lipunhintasi on {ticketPrise - ticketPrise * discount3}€");
-                else if ei
-                    Console.WriteLine($"Lipunhintasi on {ticketPrise - ticketPrise * discount4}€");
+            if (userInput == "k")
+                discount = 0.50;
+
+            else
+            {
+                Console.WriteLine("Oletko opiskelija? k tai e");
+                userInput = Console.ReadLine();
+                if (userInput == "k")
+                {
+                    Console.WriteLine("Oletko MTK:n jäsen? k tai e");
+                    userInput = Console.ReadLine();
+                    if (userInput == "k")
+                        discount = 0.60;
+                    else
+                        discount = 0.45;
+                }
+                else
+                {
+                    Console.WriteLine("Syötä ikä: ");
+                    String userInput1 = Console.ReadLine();
+                    int age = 0;
+                    age = int.Parse(userInput1);
+
+                    if (age < 7)
+                    {
+                        discount = 1.0;
+                    }
+                    else if (age >= 7 && age <= 15 || age > 65) ;
+                    {
+                        discount = 0.50;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Oletko MTK:n jäsen? k tai e");
+                        userInput = Console.ReadLine();
+                        if (userInput == "k")
+                        {
+                            discount = 0.15;
+                        }
+                    }
+                }
             }
-            if ei
-                Console.WriteLine("Oletko Varusmies?");
-            String userInput = Console.ReadLine();
-            int kylla = int.Parse(userInput);
-            if kylla
-                Console.WriteLine($"Lipunhintasi on {ticketPrise - ticketPrise * discount2}€");
-            else if ei
-                Console.WriteLine("Oletko MTK:n jäsen?");
-            String userInput = Console.ReadLine();
-            int kylla = int.Parse(userInput);
-            if kylla
-                Console.WriteLine($"Lipunhintasi on {ticketPrise - ticketPrise * discount4}€");
-            else if ei
-                Console.WriteLine($"Lipunhintasi on {ticketPrise}€");
+            Console.WriteLine($"Lipunhinta hinta on {TicketPrise - TicketPrise * discount}");
+
         }
     }
 }
