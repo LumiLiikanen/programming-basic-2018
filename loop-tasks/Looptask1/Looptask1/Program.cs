77d21f3aed59fa5f586147f0c07196e85ee67b33
Lumi Liikanen <lumi_liikanen@hotmail.com>
2018-10-11 13:54:06 +0300

    LoopTask 3.1 done

---
+++ loop-tasks/Looptask1/Looptask1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Looptask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma laskee luvun kertoman!");
+            int number = 0;
+            int i = 1;
+            int fact = 1;
+            do
+            {
+                Console.WriteLine("syötä luku!");
+                String userInput = Console.ReadLine();
+                number = int.Parse(userInput);
+                //number = int.Parse(console.ReadLine());
+
+                if (number <= 0)
+                {
+                    Console.WriteLine("Väärä syöte!");
+                }
+
+            } while (number <= 0);
+
+            while (i <= number)
+            {
+                fact = fact * i; //fact*i
+                i = i + 1; //i+1
+            }
+            Console.WriteLine($"Luvun {number}! = {fact}");
+
+
+
+
+
+        }
+    }
+}
