0c8e573cf723bc45ccce29c7a7fbc7de31a874d8
Lumi Liikanen <lumi_liikanen@hotmail.com>
2018-10-11 13:52:18 +0300

    LoopTask 4.1 done

---
+++ loop-tasks/LoopTask4-1/Looptask4.1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LoopTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+                int rndNumber;
+            //rnd.NextDouble();
+
+
+                 Console.WriteLine("Ohjelma tulostaa kokonaisluvut 1-10 ja niiden neliöjuuret!");
+            for (int i = 0; i < 10; i++)
+            {
+                rndNumber = rnd.Next(2,100);
+                Console.WriteLine($"{i+1}. {rndNumber} {Math.Sqrt(rndNumber)}");
+            }
+            {
+
+            }
+        }
+    }
+}
+;
