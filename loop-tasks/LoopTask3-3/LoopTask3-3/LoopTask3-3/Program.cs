22a39d0d2c6e0a190caf79c34ab1cf0f6225af77
Lumi Liikanen <lumi_liikanen@hotmail.com>
2018-10-17 15:12:59 +0300

    Program done!

---
+++ loop-tasks/LoopTask3-3/LoopTask3-3/LoopTask3-3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace LoopTask3_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma laskee syötettyä lukua pienempien parillisten ja parittomien positiivisten kokonaislukujen summat erikseen!");
+            int number = 0;
+            int i = 1;
+            int oddsum = 0;
+            int evensum = 0;
+
+            do
+            {
+                Console.WriteLine("Syötä luku!");
+                String userInput = Console.ReadLine();
+                number = int.Parse(userInput);
+                if (number <= 0)
+                {
+                    Console.WriteLine("Väärä syöte!");
+                }
+
+            } while (number <= 0);
+
+            while (i <= number)
+            {
+                if (i % 2 == 0)
+                {
+                    evensum = evensum + i;
+                    i = i + 1;
+                }
+
+                else 
+                {
+                    oddsum = oddsum + i;
+                    i = i + 1;
+                }
+            } 
+
+            Console.WriteLine($"Parillisten summa = {evensum} ja Parittomien summa = {oddsum}!");
+        }
+    }
+}
