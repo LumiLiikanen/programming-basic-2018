13056d7c4b1f0ecc4b84dc5fd0123e7777014bbd
Lumi Liikanen <lumi_liikanen@hotmail.com>
2018-10-28 20:32:17 +0200

    Program 4-6 done!

--- loop-tasks/LoopTask4-6/LoopTask4-6/LoopTask4-6/Program.cs
+++ loop-tasks/LoopTask4-6/LoopTask4-6/LoopTask4-6/Program.cs
@@ -9,14 +9,21 @@ namespace LoopTask4_6
             Console.WriteLine("Ohjelma tulostaa nopan heiton tuloksen 1000 kertaa ja laskee kuinka monta 6:sta heitettiin!");
             Random rnd = new Random();
             int number = 1;
+            int numberSix = 0;
             for (int i = 1; i <= 1000 ; i++)
             {
                 Console.Write($"{i}: ");
 
                         number = rnd.Next(1,7);
                         Console.WriteLine($"{number}");
+
+                int x = rnd.Next(1, 7);
+                    if (x == 6)
+                    {
+                       numberSix++;
+                    }
             }
-            Console.WriteLine($"Numero 6 heitettiin {} kertaa");
+                  Console.WriteLine($"Numero 6 heitettiin {numberSix} kertaa");
         }
     }
 }
