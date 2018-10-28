c95972dcd40f1fc9c03eb47da1b721b61a794251
Lumi Liikanen <lumi_liikanen@hotmail.com>
2018-10-28 15:46:06 +0200

    Itsearviointi 1 done

---
+++ Itsearviointi/1/1/1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma tulostaa annetun lauseen viisi kertaa!");
+            Console.WriteLine("Kirjoita lause!");
+            string userInput = Console.ReadLine();
+
+            for (int i = 0; i <= 4; i++)
+                Console.WriteLine($"{i}. {userInput}");
+
+        }
+    }
+}
