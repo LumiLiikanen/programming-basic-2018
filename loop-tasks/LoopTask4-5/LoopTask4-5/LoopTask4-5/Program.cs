3d6d724660b86c0ef72b1538986e503fb77a4abc
Lumi Liikanen <lumi_liikanen@hotmail.com>
2018-10-28 20:25:02 +0200

    program 4-5 done!

---
+++ loop-tasks/LoopTask4-5/LoopTask4-5/LoopTask4-5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LoopTask4_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            Console.WriteLine("Ohjlema tulostaa vakioveikkauksen rivin");
+
+            for (int j = 1; j <= 13; j++)
+            {
+                double number = rnd.NextDouble();
+           
+                if (number >= 0 && number <= 0.4)
+                {
+                    Console.WriteLine($"{j}. 1");
+                }
+                else if (number > 0.4 && number <= 0.6)
+                {
+                    Console.WriteLine($"{j}. X");
+                }
+                else
+                {
+                    Console.WriteLine($"{j}. 2");
+                }
+            }
+        }
+
+    }
+}
---
+++ loop-tasks/LoopTask4-6/LoopTask4-6/LoopTask4-6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LoopTask4_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma tulostaa nopan heiton tuloksen 1000 kertaa ja laskee kuinka monta 6:sta heitettiin!");
+            Random rnd = new Random();
+            int number = 1;
+            for (int i = 1; i <= 1000 ; i++)
+            {
+                Console.Write($"{i}: ");
+
+                        number = rnd.Next(1,7);
+                        Console.WriteLine($"{number}");
+            }
+            Console.WriteLine($"Numero 6 heitettiin {} kertaa");
+        }
+    }
+}
