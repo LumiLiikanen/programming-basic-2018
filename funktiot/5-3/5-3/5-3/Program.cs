182372cbdd3fcf3cc6d0e60e7b33350842bbcd41
Lumi Liikanen <lumi_liikanen@hotmail.com>
2018-10-28 22:35:23 +0200

    Program 5-3 Done

---
+++ funktiot/5-3/5-3/5-3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _5_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.WriteLine("Ohjelma pyytää käyttäjältä väliltä 1-10 ja palauttaa sallitun luvun!");
+                int number = numberFromRange(1, 10);
+                Console.WriteLine($"Luku {number} on sallittu!");
+            }
+        
+            int numberFromRange(int lowerbound, int upperbound)
+            {
+                int allowedNumber;
+                while (true)
+                {
+                    Console.WriteLine("Syötä luku!");
+                    bool isnumber = int.TryParse(Console.ReadLine(), out int userInput);
+
+                    if (userInput >= lowerbound && userInput <= upperbound)
+                    {
+                        allowedNumber = userInput;
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Luku ei ole sallittu");
+                    }
+
+                }
+                return allowedNumber;
+            }
+        }
+    }
+}
---
+++ funktiot/5-4/5-4/5-4/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace _5_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            {
+                int maxIndex = 0;
+                Console.WriteLine("Ohjelma pyytää 10 lukua ja palauttaa niistä suurimman!");
+                Console.WriteLine($"Suurin luku oli {Numbers(ref maxIndex)} ja se oli {maxIndex}.");
+                Console.ReadKey();
+            }
+            int Numbers(ref int maxIndex)
+            {
+                int nbr = 0;
+                int maxNbr = 0;
+
+                bool isNumber;
+                string output = "";
+                Console.WriteLine("Syötä 10 positiivista kokonaislukua!");
+
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.Write($"{i + 1}. ");
+                    isNumber = int.TryParse(Console.ReadLine(), out nbr);
+                    if (isNumber && nbr > 0)
+                    {
+                        output += $"{nbr} ";
+                        if (maxNbr < nbr)
+                        {
+                            maxNbr = nbr;
+                            maxIndex = i + 1;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Väärä syöte, syötä positiivinen kokonaisluku!");
+                        i--;
+                    }
+                }
+                Console.WriteLine($"Syötit seuraavat luvut:\n{output}\n");
+                return maxNbr;
+            }
+        }
+
+    }
+}
