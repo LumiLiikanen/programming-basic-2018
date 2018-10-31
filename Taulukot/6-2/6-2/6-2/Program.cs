9d3f141c14c1d45b836d8d4c19329e3caa9e5310
Lumi Liikanen <lumi_liikanen@hotmail.com>
2018-10-31 14:32:29 +0200

    Program 6-2 done!

---
+++ Taulukot/6-2/6-2/6-2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _6_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma arpoo 100 lukua väliltä 1-50 ja talukoi ne sekä ilmoittaa keskiarvon!");
+            double [] it = new double [100];
+            double sum = 0;
+            double average = 0;
+            Random rnd = new Random();
+
+            for (int i = 0; i < it.Length; i++)
+            {
+                it[i] = rnd.Next(51);
+            }
+
+           for (int i = 0; i< it.Length; i++)
+            {
+                sum = sum + it[i];
+            }
+
+                average = sum / 100;
+
+            for (int i = 0; i < it.Length; i++)
+            {
+                Console.WriteLine($"{i+1}. {it[i]}");
+            }
+            Console.WriteLine($"Keskiarvo: {average}");
+        }
+    }
+}
