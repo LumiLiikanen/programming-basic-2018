b4897ef13312407c14252ef6e919243b4214b1ad
Lumi Liikanen <lumi_liikanen@hotmail.com>
2018-10-31 13:03:05 +0200

    esimerkki done!

---
+++ Taulukot/esimerkki/esimerkki/esimerkki/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace esimerkki
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma alustaa kokonaisluku taulukon (10alkiota), johon arvotaan lukuja väliltä 0-100");
+            int[] numbers = new int[10];
+            Random rnd = new Random();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = rnd.Next(100);
+            }
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+            Console.WriteLine("Ohjelman suoritus päättynyt");
+        }
+    }
+}
