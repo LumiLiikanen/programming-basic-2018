fb14619920e348a595d301bbeaa7856fe7c735ec
Lumi Liikanen <lumi_liikanen@hotmail.com>
2018-11-08 10:49:30 +0200

    Lottery Program done!

---
+++ Taulukot/Lotto/lotto/lotto/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace lotto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma arpoo lottonumerot!");
+            int[] lottery = new int[40];
+            Random rnd = new Random();
+
+            for (int i = 0; i < 7; i++)
+            {
+                int rndnbr = rnd.Next(40);
+
+                    if (lottery[rndnbr] == 0)
+                    {
+                        lottery[rndnbr] = 1;
+                    }
+                    else
+                    {
+                        i--;
+                    }
+            }
+
+            while (true)
+            {
+                int rndnbr = rnd.Next(40);
+                if (lottery[rndnbr] == 0)
+                {
+                    lottery[rndnbr] = 2;
+                    break;
+                }
+            }
+            Console.WriteLine("Arvottiin oikeat luvut");
+            for (int i = 0; i < lottery.Length; i++)
+                if (lottery[i] == 1)
+                {
+                    Console.Write($"{ i + 1} ");
+                }
+
+            for (int i = 0; i < lottery.Length; i++)
+                if (lottery[i] == 2)
+                {
+                    Console.WriteLine($"\nLisänumero: { i + 1} ");
+                }
+
+            Console.WriteLine($"plusnumero: {rnd.Next(40) + 1}");
+        }
+    }
+}
