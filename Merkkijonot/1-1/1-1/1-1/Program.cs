e42838c8fb3e6718743676fba0ea5405a87b1779
Lumi Liikanen <lumi_liikanen@hotmail.com>
2018-11-12 11:28:51 +0200

    Program 1-1 done!

---
+++ Merkkijonot/1-1/1-1/1-1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma laskee syötettyjen merkkien määrän!");
+            Console.WriteLine("Syötä lause!");
+            string userInput = Console.ReadLine();
+            int k = userInput.Length;
+
+                Console.WriteLine($"Lauseessa on: {k} merkkiä!");
+        }
+    }
+}
