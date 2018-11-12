58e76cee90cdaa220bf9a2f0d3e6aa4ca49e4e39
Lumi Liikanen <lumi_liikanen@hotmail.com>
2018-11-12 11:30:33 +0200

    Program 1-2 done!

---
+++ Merkkijonot/1-2/1-2/1-2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma muuttaa e -kirjaimet @ -merkeiksi!");
+            Console.WriteLine("Syötä lause!");
+            string userInput = Console.ReadLine();
+            string temp = userInput.Replace('e', '@');
+            Console.WriteLine($"Syötetty lause: {userInput}\nKorvattu lause: {temp}");
+        }
+    }
+}
