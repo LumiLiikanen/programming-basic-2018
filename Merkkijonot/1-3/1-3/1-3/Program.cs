1b9b7f32c9314b690755e2b6f63ea7f577760fd8
Lumi Liikanen <lumi_liikanen@hotmail.com>
2018-11-21 10:53:50 +0200

    Program 1-3 done!

---
+++ Merkkijonot/1-3/1-3/1-3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _1_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma laskee syötetystä lauseesta l -kirjainten määrän!");
+            Console.WriteLine("Syötä lause!");
+            string userInput = Console.ReadLine();
+            userInput = userInput.ToUpper();
+            int LetterCounter = 0;
+
+            for (int i = 0; i < userInput.Length; i++)
+            {
+                if (userInput[i].ToString() == "L")
+                    LetterCounter++;
+            }
+            Console.WriteLine($"L -kirjaimien määrä {LetterCounter}");
+        }
+    }
+}
