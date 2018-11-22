e5e04d252b1d6ed89e69b47d6ab0de7951f252dd
Lumi Liikanen <lumi_liikanen@hotmail.com>
2018-11-22 09:24:26 +0200

    Program FunktiotJaTaulukot1 done!

---
+++ Itsearviointi/TaulukkoJaFunktio1/TaulukkoJaFunktio1/TaulukkoJaFunktio1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace TaulukkoJaFunktio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma tulostaa tähtiä");
+            int [] numbers = new int[] { 2, 5, 10, 7, 3 };
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+               Console.WriteLine(GetStars(numbers[i]));
+            }
+        }
+
+    
+        static void PrintStars (int length)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+
+        static string GetStars (int length)
+        {
+            string returnStars = "";
+            for (int i = 0; i < length; i++)
+            {
+                returnStars += "*";
+            }
+            return returnStars;
+        }
+    }
+}
