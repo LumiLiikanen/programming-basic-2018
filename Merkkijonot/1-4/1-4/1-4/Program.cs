5899323799e65b450d7f1611ea9d461a87f0482f
Lumi Liikanen <lumi_liikanen@hotmail.com>
2018-11-27 08:45:50 +0200

    Program 1-4 done!

---
+++ Merkkijonot/1-4/1-4/1-4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _1_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma kertoo onko syötetty sana tai lause palintromi!");
+            Console.WriteLine("Syötä sana tai lause!");
+            string userInput = Console.ReadLine();
+            String userInputtmp = userInput.ToUpper();
+            userInputtmp = userInput.Replace(" ", "");
+
+            for (int i = 0; i < userInputtmp.Length; i++)
+            {
+                if (userInputtmp[i] != userInputtmp[userInputtmp.Length - 1 - i])
+                {
+                    Console.WriteLine($"{userInput} ei ole palintromi");
+                    break;
+                }
+
+                else
+                {
+                    Console.WriteLine($"{userInput} on palintromi!");
+                    break;
+                }
+            }
+
+        }
+    }
+}
