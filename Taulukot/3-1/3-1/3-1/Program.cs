443899bee0fac160cbff60307944e0e8bc0efa6f
Lumi Liikanen <lumi_liikanen@hotmail.com>
2018-12-03 10:57:14 +0200

    Program 3-1 done!

---
+++ Taulukot/3-1/3-1/3-1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma arpoo yksiulotteiseen taulukkoon luvut 0-19!");
+            int[] numbers = new int[10];
+            fun(ref numbers);
+            Random rnd = new Random();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = rnd.Next(20);
+            }
+        }
+        static void fun(ref int[] numbers)
+        {
+             Random rand = new Random(); 
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = rand.Next(20);
+                Console.WriteLine($"[{i:d2}]: {numbers[i]}");
+            }
+        }
+    }
+}
